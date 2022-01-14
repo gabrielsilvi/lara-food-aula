7b2094eaeb96e17e5f0abbe9553f2f951de5c8e2
Gabriel Silvino <gabrielsv3000@hotmail.com>
2022-01-13 21:53:29 -0300

    listando e paginando planos

--- app/Http/Controllers/Admin/PlanController.php
+++ app/Http/Controllers/Admin/PlanController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Plan;
 use Illuminate\Http\Request;
+
 
 class PlanController extends Controller
 {
+    private $repoistory;
+
+    public function __construct(Plan $plan)
+    {
+       $this->repoistory = $plan;
+    }
+
     public function index(){
-        
-        return view('admin.pages.plans.index');
+        $plans = $this->repoistory->paginate();
+        return view('admin.pages.plans.index', ['plans' => $plans,]); 
+
     }
 }
--- resources/views/admin/pages/plans/index.blade.php
+++ resources/views/admin/pages/plans/index.blade.php
@@ -8,4 +8,39 @@
 
 @section('content')
     <p>Listagem de Planos</p>
+    <div class="card">
+
+        <div class="card-header">
+            #filtros
+        </div>
+        <div class="card-body">
+            <table class="table table-condensed">
+                <thead>                   
+                    <tr>
+                        <th>Nome</th>
+                        <th>Preco</th>
+                        <th width="50">Acoes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($plans as $plan)
+                        <tr>
+                            <td>
+                                {{$plan->name}}
+                            </td>
+                            <td>
+                                {{$plan->price}}
+                            </td>
+                            <td>
+                                <a href="" class="btn btn-warning">Visualizar</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+            {{ $plans->links() }}
+        </div>
+    </div>
 @stop
