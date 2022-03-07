7e4f69729953118203cb7e9fb91a7474347eaaec
Gabriel Silvino <gabrielsv3000@hotmail.com>
2022-03-07 01:23:59 -0300

    validacao de profiles

--- app/Http/Controllers/Admin/ACL/ProfileController.php
+++ app/Http/Controllers/Admin/ACL/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin\ACL;
 
+use App\Http\Requests\StoreUpdateProfile;
 use App\Http\Controllers\Controller;
 use App\Models\Profile;
 use Illuminate\Http\Request;
@@ -36,10 +37,10 @@ class ProfileController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\StoreUpdateProfile  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreUpdateProfile $request)
     {
        $this->repository->create($request->all());
 
@@ -74,11 +75,11 @@ class ProfileController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\StoreUpdateProfile  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(StoreUpdateProfile $request, $id)
     {
         if(!$profile = $this->repository->find($id)){
             return redirect()->back();
